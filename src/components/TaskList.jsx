00a9f431ac7c75bc2f47cf7d88d9fb61eebff7c5
HtetMinMyat <htetminmyat1511@gmail.com>
2025-03-25 20:27:34 +0700

    update done list count

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -5,7 +5,7 @@ const TaskList = ({tasks,removeTask,doneTask}) => {
   return (
     <div>
       <h1 className="text-2xl mb-3">
-        Task list (<span>{tasks.length}</span>) , Done (<span>0</span>)
+        Task list (<span>{tasks.length}</span>) , Done (<span>{tasks.filter(el => el.isDone).length}</span>)
       </h1>
       {tasks.map(el => <Task doneTask={doneTask} removeTask={removeTask} key={el.id} job={el} />)}
     </div>
